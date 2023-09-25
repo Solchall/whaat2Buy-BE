17f196b679706edb860c0bf0992062d188097c1a
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-09-26 02:22:14 +0900

    fix: change accessToken api method from post to get

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -10,7 +10,7 @@ router.post("/signup", controllers.auth.signup);
 router.post("/login", controllers.auth.login);
 router.post("/logout", controllers.auth.logout);
 router.post("/logoutAll", controllers.auth.logoutAll);
-router.post("/accessToken", controllers.auth.newAccessToken);
+router.get("/accessToken", controllers.auth.newAccessToken);
 router.post("/refreshToken", controllers.auth.newRefreshToken);
 
 module.exports = router;
